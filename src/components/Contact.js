99e9e5ccaf71b4b90b6fe7786b4b416051323633
Kevin Lopez <kevin_lopez@live.com>
2021-02-25 15:11:28 -0600

    adds links to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,12 +6,16 @@ const Contact = () => {
             <h3>Contact me</h3>
             <span>kevin_lopez@live.com</span>
             <div className="social-media">
+            <a href="https://github.com/kevlpz" target="_blank">
                 <div className="social-media-icon-container">
                     <span><IoLogoOctocat /></span>
                 </div>
-                <div className="social-media-icon-container">
-                    <span><IoLogoLinkedin /></span>
-                </div>
+            </a>
+                <a href="https://www.linkedin.com/in/kevin-lopez-03a074a9/" target="_blank">
+                    <div className="social-media-icon-container">
+                        <span><IoLogoLinkedin /></span>
+                    </div>
+                </a>
             </div>
         </div>
     )
